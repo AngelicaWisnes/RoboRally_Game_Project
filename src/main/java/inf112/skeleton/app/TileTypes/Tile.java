4f5d4dc41cdf743ec34fb4ccfccef6eaa26f664f
= <=>
2019-02-06 14:57:40 +0100

    Created iTile, and abstract Tile

--- src/main/java/inf112/skeleton/app/TileTypes/Tile.java
+++ src/main/java/inf112/skeleton/app/TileTypes/Tile.java
@@ -1,55 +1,16 @@
 package inf112.skeleton.app.TileTypes;
 
-enum TileType {
-    GEAR,
-    REPAIR,
-    PIT,
-    EDGE,
-    STARTINGPOINT,
-    LASER,
-    WALL,
-}
+/**
+ * @author Magne
+ */
+public class Tile implements iTile{
 
-public class Tile {
-
-    private final Tile tileType;
-    private final Direction rotation;
-    private final int x;
-    private final int y;
-
-    public Tile(Tile tileType, int x, int y, Direction rotation) {
-        this.tileType = tileType;
-        this.x = x;
-        this.y = y;
-        this.rotation = rotation;
-    }
-
-    public Tile getTileType() {
-        return tileType;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public Direction getRotation() {
-        return rotation;
-    }
-
-    //public getImage() {
-
-    //}
-
-    public static enum Direction {
-        NORTH,
-        EAST,
-        SOUTH,
-        WEST;
+    public Tile() {
 
     }
 
+    @Override
+    public String getImage() {
+        return null;
+    }
 }
