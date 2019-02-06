778eb8c7bcd367d124d365471e721c1b0ac6df45
RogerWisnes <rogerwisnes@gmail.com>
2019-02-06 16:16:11 +0100

    Created Edge-tile

---
+++ src/main/java/inf112/skeleton/app/TileTypes/Edge.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app.TileTypes;
+
+/**
+ * @author Roger Wisnes
+ */
+public class Edge extends Tile {
+
+    public Edge() {
+        this.image = "edge";
+        this.size = 1;
+
+
+    }
+
+    @Override
+    public String getImage() {
+        return image;
+    }
+}
