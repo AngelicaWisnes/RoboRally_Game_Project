5c4b0f4485ffb3cb440adf4ab1f350867c8d13cc
RogerWisnes <rogerwisnes@gmail.com>
2019-02-06 15:58:19 +0100

    Created pit-tile

---
+++ src/main/java/inf112/skeleton/app/TileTypes/Pit.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app.TileTypes;
+
+/**
+ * @author Roger Wisnes
+ */
+public class Pit extends Tile {
+
+    public Pit() {
+        this.image = "pit";
+        this.size = 1;
+
+
+    }
+
+    @Override
+    public String getImage() {
+        return image;
+    }
+}
