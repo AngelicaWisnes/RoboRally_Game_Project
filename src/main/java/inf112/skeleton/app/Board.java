d72f25ef4bbc8704d1b2f96e8b3aeb83f6782480
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-02-07 13:42:04 +0100

    code review board

--- src/main/java/inf112/skeleton/app/Board.java
+++ src/main/java/inf112/skeleton/app/Board.java
@@ -1,7 +1,6 @@
 package inf112.skeleton.app;
 
 import java.util.Random;
-
 import inf112.skeleton.app.TileTypes.Tile;
 import inf112.skeleton.app.TileTypes.*;
 
@@ -30,7 +29,6 @@ public class Board {
         return BOARDSIZE_Y;
     }
 
-    //TODO create standard Tile board
     private void populateTileBoard() {
         Random r = new Random();
         for (int i = 0; i < BOARDSIZE_X; i++) {
@@ -66,7 +64,7 @@ public class Board {
     }
 
 
-    //board based on input
+    //TODO: board based on input
     private void populateBoard(String input) {
 
     }
