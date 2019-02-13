bb0efae5c5fc168f5aaf69f68be4e7c02dea438c
RogerWisnes <rogerwisnes@gmail.com>
2019-02-13 15:23:10 +0100

    Created AbstractRobot, and filled in method rotate(...)

--- src/main/java/inf112/skeleton/app/AbstractRobot.java
+++ src/main/java/inf112/skeleton/app/AbstractRobot.java
@@ -1,11 +1,26 @@
 package inf112.skeleton.app;
 
 import inf112.skeleton.app.Enums.Direction;
+import inf112.skeleton.app.Enums.Rotation;
 
 public abstract class AbstractRobot implements iRobot {
-    Position pos;
-    Direction dir;
+    protected Position pos;
+    private Direction dir;
 
+    protected AbstractRobot() {
 
+    }
 
+    @Override
+    public Direction rotate(Rotation rotation) {
+        if (rotation.equals(Rotation.TURN_AROUND)) return dir = dir.opposite();
+        if (rotation.equals(Rotation.TURN_CLOCKWISE)) return dir = dir.clockwise();
+        if (rotation.equals(Rotation.TURN_COUNTER_CLOCKWISE)) return dir = dir.counterClockwise();
+        throw new IllegalArgumentException("Must have valid rotation-input to rotate robot");
+    }
+
+    @Override
+    public Position move(Direction direction, int spaces) {
+        return null;
+    }
 }
