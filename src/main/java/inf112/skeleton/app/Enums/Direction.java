2d94bdb2ce86a8ea463bb3728da3971994b7f560
RogerWisnes <rogerwisnes@gmail.com>
2019-02-13 15:04:20 +0100

    Added rotational methods in Enum Direction

--- src/main/java/inf112/skeleton/app/Enums/Direction.java
+++ src/main/java/inf112/skeleton/app/Enums/Direction.java
@@ -2,5 +2,38 @@ package inf112.skeleton.app.Enums;
 
 
   public enum Direction {
-        UP, DOWN, LEFT, RIGHT
+        UP, DOWN, LEFT, RIGHT;
+
+      public Direction opposite() {
+          switch (this) {
+              case UP: return Direction.DOWN;
+              case DOWN: return Direction.UP;
+              case LEFT: return Direction.RIGHT;
+              case RIGHT: return Direction.LEFT;
+              default: throw new IllegalArgumentException(
+                      "Direction " + this + " has no opposite.");
+          }
+      }
+
+      public Direction clockwise() {
+          switch (this) {
+              case UP: return Direction.RIGHT;
+              case RIGHT: return Direction.DOWN;
+              case DOWN: return Direction.LEFT;
+              case LEFT: return Direction.UP;
+              default: throw new IllegalArgumentException(
+                      "Direction " + this + " has no clockwise turn-direction.");
+          }
+      }
+
+      public Direction counterClockwise() {
+          switch (this) {
+              case UP: return Direction.LEFT;
+              case LEFT: return Direction.DOWN;
+              case DOWN: return Direction.RIGHT;
+              case RIGHT: return Direction.UP;
+              default: throw new IllegalArgumentException(
+                      "Direction " + this + " has no counterclockwise turn-direction.");
+          }
+      }
     }
