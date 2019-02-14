e3171a966f3ac3b7fc92a796da1b91eaf87fb6d1
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-02-14 11:37:35 +0100

    added Slot object to ProgramSheet

--- src/main/java/inf112/skeleton/app/ProgramSheet.java
+++ src/main/java/inf112/skeleton/app/ProgramSheet.java
@@ -2,13 +2,23 @@ package inf112.skeleton.app;
 
 import inf112.skeleton.app.Robot.*;
 
+import java.util.ArrayList;
+
 public class ProgramSheet {
     private iRobot robot;
     private int damage;
     //Checkpoint checkpoint;
     private int lives;
+    private ArrayList<Card> cards;
+    private Slot slot1;
+    private Slot slot2;
+    private Slot slot3;
+    private Slot slot4;
+    private Slot slot5;
 
     public ProgramSheet(int playerN, Board board){
         robot = new Robot(playerN, board);
+        this.damage = 0;
+        this.lives = 3;
     }
 }
