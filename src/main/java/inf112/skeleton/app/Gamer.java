e1aa20eb369958f53c4cc8014a2c0fe0861d44db
Ingrid Eriksen <ingrid.regine.eriksen@student.uib.no>
2019-02-25 12:14:27 +0100

    added tiledmap to the gamer

--- src/main/java/inf112/skeleton/app/Gamer.java
+++ src/main/java/inf112/skeleton/app/Gamer.java
@@ -1,13 +1,16 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import inf112.skeleton.app.Robot.iRobot;
 
 public class Gamer {
 
-    private final iRobot robot;
+    private TiledMap map;
     private final Board board;
+    private final iRobot robot;
 
-    public Gamer(Board board, iRobot robot) {
+    public Gamer(TiledMap map, Board board, iRobot robot) {
+        this.map = map;
         this.board = board;
         this.robot = robot;
     }
