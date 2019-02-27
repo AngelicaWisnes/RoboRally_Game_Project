2201c6ae0f909f51a7f3b0d1c250bbb3dc23b7db
RogerWisnes <rogerwisnes@gmail.com>
2019-02-27 15:14:10 +0100

    Missing files from last commit

--- src/main/java/inf112/skeleton/app/Robot/AbstractRobot.java
+++ src/main/java/inf112/skeleton/app/Robot/AbstractRobot.java
@@ -1,8 +1,13 @@
 package inf112.skeleton.app.Robot;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import inf112.skeleton.app.Enums.Direction;
 import inf112.skeleton.app.Enums.Rotation;
 import inf112.skeleton.app.Position;
+import inf112.skeleton.app.Screens.GameScreen;
 
 /**
  * @author Roger Wisnes
@@ -10,14 +15,49 @@ import inf112.skeleton.app.Position;
 public abstract class AbstractRobot implements iRobot {
     private Position pos;
     private Direction dir;
+    private int playerID, width, height;
+
 
     protected AbstractRobot(Position pos, Direction dir) {
         this.pos = pos;
+        this.width = GameScreen.TILESIZE;
+        this.height = GameScreen.TILESIZE;
+
         this.dir = dir;
     }
 
-    @Override
-    public Direction rotate(Rotation rotation) {
+    public Position getPos() { return pos; }
+
+    public void keyboardMoveRobot(TiledMap map) {
+        //move the robot one tile in a direction
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) move(Direction.LEFT, 1);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) move(Direction.RIGHT, 1);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) move(Direction.UP, 1);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) move(Direction.DOWN, 1);
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)
+                || Gdx.input.isKeyJustPressed(Input.Keys.LEFT)
+                || Gdx.input.isKeyJustPressed(Input.Keys.UP)
+                || Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            int x = pos.getX() / GameScreen.TILESIZE;
+            int y = pos.getY() / GameScreen.TILESIZE;
+            try {
+                TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) map.getLayers().get(0)).getCell(x, y);
+                System.out.println(cell.getTile().getId());
+
+            } catch (Exception e) {
+                System.out.println("du har dødd");
+            }
+
+        }
+    }
+
+    /**
+     * Rotate the robot
+     * @param rotation the rotation-Enum describing how turning-direction
+     * @return the new direction
+     */
+    private Direction rotate(Rotation rotation) {
         switch (rotation) {
             case TURN_CLOCKWISE: return dir = dir.clockwise();
             case TURN_COUNTER_CLOCKWISE: return dir = dir.counterClockwise();
@@ -27,25 +67,22 @@ public abstract class AbstractRobot implements iRobot {
         }
     }
 
-    @Override
-    public Position move(Direction direction, int spaces) {
+    /**
+     * Move the robot in the given direction
+     * @param direction given from keyboardMoveRobot
+     * @param spaces number of spaces to move
+     * @return the new position
+     */
+    private Position move(Direction direction, int spaces) {
         if (spaces < 1 || spaces > 3){
             throw new IllegalArgumentException("Must have valid spaces-input to move robot");
         }
 
         switch (direction) {
-            case UP:
-                pos.y -= spaces;
-                return pos;
-            case DOWN:
-                pos.y += spaces;
-                return pos;
-            case LEFT:
-                pos.x -= spaces;
-                return pos;
-            case RIGHT:
-                pos.x += spaces;
-                return pos;
+            case UP: pos.setY(spaces * GameScreen.TILESIZE); return pos;
+            case DOWN: pos.setY(-spaces * GameScreen.TILESIZE); return pos;
+            case LEFT: pos.setX(-spaces * GameScreen.TILESIZE); return pos;
+            case RIGHT: pos.setX(spaces * GameScreen.TILESIZE); return pos;
             default:
                 throw new IllegalArgumentException("Must have valid direction-input to move robot");
         }
--- src/main/java/inf112/skeleton/app/Robot/iRobot.java
+++ src/main/java/inf112/skeleton/app/Robot/iRobot.java
@@ -1,23 +1,13 @@
 package inf112.skeleton.app.Robot;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import inf112.skeleton.app.Enums.Direction;
 import inf112.skeleton.app.Enums.Rotation;
 import inf112.skeleton.app.Position;
 
 public interface iRobot {
 
-    /**
-     * Rotate the robot
-     * @param rotation the rotation-Enum describing how turning-direction
-     * @return the new direction
-     */
-    Direction rotate(Rotation rotation);
+    Position getPos();
 
-    /**
-     * Move the robot in the given direction
-     * @param direction given from Program-card or Game-board
-     * @param spaces number of spaces to move
-     * @return the new position
-     */
-    Position move(Direction direction, int spaces);
+    void keyboardMoveRobot(TiledMap map);
 }
