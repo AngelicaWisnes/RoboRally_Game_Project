10dbd55d9f52f0979a2a53b932388fb84da9c04c
Ingrid Eriksen <ingrid.regine.eriksen@student.uib.no>
2019-02-27 14:51:09 +0100

    removed Board from gamer

--- src/main/java/inf112/skeleton/app/Gamer.java
+++ src/main/java/inf112/skeleton/app/Gamer.java
@@ -6,12 +6,10 @@ import inf112.skeleton.app.Robot.iRobot;
 public class Gamer {
 
     private TiledMap map;
-    private final Board board;
     private final iRobot robot;
 
-    public Gamer(TiledMap map, Board board, iRobot robot) {
+    public Gamer(TiledMap map, iRobot robot) {
         this.map = map;
-        this.board = board;
         this.robot = robot;
     }
 }
