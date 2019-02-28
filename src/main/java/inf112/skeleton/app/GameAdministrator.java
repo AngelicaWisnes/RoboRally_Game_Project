5e8cec4b44d207b714b906569df73ce41739f83d
RogerWisnes <rogerwisnes@gmail.com>
2019-02-28 13:19:44 +0100

    Added methods returnCardsToDealer and reuseTrashedCards

--- src/main/java/inf112/skeleton/app/GameAdministrator.java
+++ src/main/java/inf112/skeleton/app/GameAdministrator.java
@@ -1,9 +1,33 @@
 package inf112.skeleton.app;
 
+import inf112.skeleton.app.Card.Card;
+import inf112.skeleton.app.Card.CardGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
 public class GameAdministrator {
 
 
-    public static void startTurn(){
+    private static Stack<Card> cardStack = new Stack<>();
+    private static Stack<Card> trashedCardStack = new Stack<>();
+    static {
+        cardStack.addAll(CardGenerator.getNewCardStack());
+        Collections.shuffle(cardStack);
+    }
+
+    public static void returnCardsToDealer(ArrayList<Card> returnedCards) {
+        trashedCardStack.addAll(returnedCards);
+    }
+
+    private static void reuseTrashedCards() {
+        cardStack.addAll(trashedCardStack);
+        Collections.shuffle(cardStack);
+        trashedCardStack.clear();
+    }
+
+    public static void startTurn() {
         //Deal the Program cards
 
         // Arrange your ProgramCards face down among your five registers
@@ -19,8 +43,7 @@ public class GameAdministrator {
         // Clean up any end-of-turn effects
     }
 
-
-    private static void dealCards(){
+    private static void dealCards() {
 
     }
 
