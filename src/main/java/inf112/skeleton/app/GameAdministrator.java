a66bb23cd980bfc7aa5090ed45bf60562ba551a5
RogerWisnes <rogerwisnes@gmail.com>
2019-02-28 12:06:23 +0100

    Added comments

--- src/main/java/inf112/skeleton/app/GameAdministrator.java
+++ src/main/java/inf112/skeleton/app/GameAdministrator.java
@@ -4,6 +4,23 @@ public class GameAdministrator {
 
 
     public static void startTurn(){
+        //Deal the Program cards
+
+        // Arrange your ProgramCards face down among your five registers
+
+        // Announce intent to power down or continue running NEXT turn
+
+        // Complete each register in order:
+        //      Execute the program
+        //      Complete board movements
+        //      Resolve all interactions
+        //      Touch flags and repair sites
+
+        // Clean up any end-of-turn effects
+    }
+
+
+    private static void dealCards(){
 
     }
 
