b851e7c9229207fd81ec0725db7788413662ca67
= <=>
2019-03-06 16:11:15 +0100

    Added 8 tests in CardTest

--- src/test/java/inf112/skeleton/app/CardTest.java
+++ src/test/java/inf112/skeleton/app/CardTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardTest {
+
     @Test
     public void cardHasRotation(){
         Card card = new RotationCard(50, Rotation.TURN_CLOCKWISE);
@@ -20,6 +21,43 @@ public class CardTest {
         Card card = new RotationCard(50, Rotation.TURN_CLOCKWISE);
         assertEquals(50, card.getPriority());
     }
+
+    @Test
+    public void forwardCardHasPriorityLow1Step() {
+        Card card = new MoveForward(490, 1);
+        assertEquals(490, card.getPriority());
+    }
+
+    @Test
+    public void forwardCardHasPriorityHigh1Step() {
+        Card card = new MoveForward(650, 1);
+        assertEquals(650, card.getPriority());
+    }
+
+    @Test
+    public void forwardCardHasPriorityLow2Steps() {
+        Card card = new MoveForward(670, 2);
+        assertEquals(670, card.getPriority());
+    }
+
+    @Test
+    public void forwardCardHasPriorityHigh2Steps() {
+        Card card = new MoveForward(780, 2);
+        assertEquals(780, card.getPriority());
+    }
+
+    @Test
+    public void forwardCardHasPriorityLow3Steps() {
+        Card card = new MoveForward(790, 3);
+        assertEquals(790, card.getPriority());
+    }
+
+    @Test
+    public void forwardCardHasPriorityHigh3Steps() {
+        Card card = new MoveForward(840, 3);
+        assertEquals(840, card.getPriority());
+    }
+
     @Test(expected = IllegalStateException.class)
     public void cardStepAttributeTest() {
         new MoveForward(55, 5);
