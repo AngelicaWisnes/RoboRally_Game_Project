24c95f05ae75858199c6d245a0fb56dbd8c69cc4
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-03-28 12:53:03 +0100

    added position tests

--- src/test/java/inf112/skeleton/app/PositionTest.java
+++ src/test/java/inf112/skeleton/app/PositionTest.java
@@ -1,7 +1,26 @@
 package inf112.skeleton.app;
+import inf112.skeleton.app.Enums.Direction;
+import inf112.skeleton.app.Helpers.Position;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PositionTest {
+
+    @Test
+    public void cloneTest(){
+        Position position = new Position(1,1);
+        Position newPosition = position.clone();
+        assertEquals(position.getX(), newPosition.getX());
+        assertEquals(position.getY(), newPosition.getY());
+    }
+
+    @Test
+    public void getNeighbourTest(){
+        Position position = new Position(1, 1);
+        Position neighbour = position.getNeighbour(Direction.UP, 1);
+        assertEquals(position.getY() + 1, neighbour.getY());
+    }
 
 
 
