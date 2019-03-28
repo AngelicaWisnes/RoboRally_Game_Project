120354616d782510109533fe09f353df9af092fb
RogerWisnes <rogerwisnes@gmail.com>
2019-03-28 12:15:20 +0100

    Added a dummy class, with headless application, to be able to test robotmovements

--- src/test/java/inf112/skeleton/app/AbstractRobotTest.java
+++ src/test/java/inf112/skeleton/app/AbstractRobotTest.java
@@ -1,106 +1,109 @@
-/*
+
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import inf112.skeleton.app.Enums.Direction;
 import inf112.skeleton.app.Enums.Rotation;
+import inf112.skeleton.app.Helpers.DummyClassesForTesting.Dummy;
 import inf112.skeleton.app.Helpers.Position;
-import inf112.skeleton.app.Robot.AbstractRobotGetSet;
 import inf112.skeleton.app.Robot.Robot;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-*/
+
 /**
  * jUnit test for simple App.
- *//*
+ */
 
 public class AbstractRobotTest {
 
-    private Position pos;
-    private TiledMap map;
+    private static Position pos;
+    private static Dummy dummy;
+
+    @BeforeClass
+    public static void setupDummy(){
+        dummy = new Dummy();
+    }
 
     @Before
-    public void createMap(){
+    public void setupPosition(){
         pos = new Position(5,5);
-        map = new TmxMapLoader().load("assets/maps/map.tmx");
     }
 
 
     @Test
     public void rotatingClockwiseFromRightShouldReturnDown() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.RIGHT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_CLOCKWISE), Direction.DOWN);
+        dummy.setRobot(new Robot(pos, Direction.RIGHT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_CLOCKWISE), Direction.DOWN);
     }
 
     @Test
     public void rotatingClockwiseFromDownShouldReturnLeft() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.DOWN, map);
-        assertEquals(robot.testRotation(Rotation.TURN_CLOCKWISE), Direction.LEFT);
+        dummy.setRobot(new Robot(pos, Direction.DOWN, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_CLOCKWISE), Direction.LEFT);
     }
 
     @Test
     public void rotatingClockwiseFromLeftShouldReturnUp() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.LEFT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_CLOCKWISE), Direction.UP);
+        dummy.setRobot(new Robot(pos, Direction.LEFT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_CLOCKWISE), Direction.UP);
     }
 
     @Test
     public void rotatingClockwiseFromUpShouldReturnRight() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.UP, map);
-        assertEquals(robot.testRotation(Rotation.TURN_CLOCKWISE), Direction.RIGHT);
+        dummy.setRobot(new Robot(pos, Direction.UP, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_CLOCKWISE), Direction.RIGHT);
     }
 
     @Test
     public void rotatingCounterclockwiseFromRightShouldReturnUp() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.RIGHT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.UP);
+        dummy.setRobot(new Robot(pos, Direction.RIGHT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.UP);
     }
 
     @Test
     public void rotatingCounterclockwiseFromDownShouldReturnRight() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.DOWN, map);
-        assertEquals(robot.testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.RIGHT);
+        dummy.setRobot(new Robot(pos, Direction.DOWN, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.RIGHT);
     }
 
     @Test
     public void rotatingCounterclockwiseFromLeftShouldReturnDown() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.LEFT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.DOWN);
+        dummy.setRobot(new Robot(pos, Direction.LEFT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.DOWN);
     }
 
     @Test
     public void rotatingCounterclockwiseFromUpShouldReturnLeft() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.UP, map);
-        assertEquals(robot.testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.LEFT);
+        dummy.setRobot(new Robot(pos, Direction.UP, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_COUNTER_CLOCKWISE), Direction.LEFT);
     }
 
     @Test
     public void rotatingAroundFromUpShouldReturnDown() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.UP, map);
-        assertEquals(robot.testRotation(Rotation.TURN_AROUND), Direction.DOWN);
+        dummy.setRobot(new Robot(pos, Direction.UP, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_AROUND), Direction.DOWN);
     }
 
     @Test
     public void rotatingAroundFromRightShouldReturnLeft() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.RIGHT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_AROUND), Direction.LEFT);
+        dummy.setRobot(new Robot(pos, Direction.RIGHT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_AROUND), Direction.LEFT);
     }
 
     @Test
     public void rotatingAroundFromDownShouldReturnUp() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.DOWN, map);
-        assertEquals(robot.testRotation(Rotation.TURN_AROUND), Direction.UP);
+        dummy.setRobot(new Robot(pos, Direction.DOWN, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_AROUND), Direction.UP);
     }
 
     @Test
     public void rotatingAroundFromLeftShouldReturnRight() {
-        AbstractRobotGetSet robot = new Robot(pos, Direction.LEFT, map);
-        assertEquals(robot.testRotation(Rotation.TURN_AROUND), Direction.RIGHT);
+        dummy.setRobot(new Robot(pos, Direction.LEFT, dummy.getMap(), dummy.getSheet()));
+        assertEquals(dummy.getRobot().testRotation(Rotation.TURN_AROUND), Direction.RIGHT);
     }
 
 }
-*/
+
