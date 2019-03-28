1c1a1cbe486509381b2c1ce1bb412163d0dfc2ed
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-03-28 15:51:03 +0100

    laser is generated on map and collision works

--- src/main/java/inf112/skeleton/app/Screens/GameScreen.java
+++ src/main/java/inf112/skeleton/app/Screens/GameScreen.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 import inf112.skeleton.app.Controller;
@@ -175,20 +176,34 @@ public class GameScreen implements Screen {
     private void fireLasers(ShapeRenderer shape) {
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.setColor(Color.RED);
+        Rectangle robotRectangle = new Rectangle(gamer.getSheet().getRobot().getPos().getX(), gamer.getSheet().getRobot().getPos().getY(), TILESIZE, TILESIZE);
+
         for (Position laser : lasers) {
-            TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) map.getLayers().get(0)).getCell(laser.getX()/64, laser.getY()/64);
+            TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) map.getLayers().get(0)).getCell(laser.getX() / 64, laser.getY() / 64);
             int id = cell.getTile().getId();
             if (id == 929) { //
-                shape.rect(laser.getX() + TILESIZE / 4 - (64*3), laser.getY() + TILESIZE / 4, 64*3, 10);
+                Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 4 - (64 * 3), laser.getY() + TILESIZE / 4, 64 * 3, 10);
+                shape.rect(laser.getX() + TILESIZE / 4 - (64 * 3), laser.getY() + TILESIZE / 4, 64 * 3, 10);
+                if (laserRect.overlaps(robotRectangle)) {
+                    System.out.println("LAZERED!");
+                }
+
+
             }
             if (id == 646) {
-                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64*3), 10, 64*3);
+                Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 10, 64 * 3);
+
+                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 10, 64 * 3);
+                if (laserRect.overlaps(robotRectangle)) {
+                    System.out.println("LAZERED!");
+                }
 
             }
         }
         shape.end();
-
     }
+
+
 
 
     private void stateBasedMovement() {
