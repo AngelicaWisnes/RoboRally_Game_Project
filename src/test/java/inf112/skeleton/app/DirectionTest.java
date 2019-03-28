e855d0693a0c8caa690807c8ca9e22384dd190fe
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-03-28 12:02:00 +0100

    added direction tests

---
+++ src/test/java/inf112/skeleton/app/DirectionTest.java
@@ -0,0 +1,38 @@
+package inf112.skeleton.app;
+import inf112.skeleton.app.Enums.Direction;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DirectionTest {
+
+    @Test
+    public void oppositeOfLeftReturnsRight() {
+        Direction direction = Direction.LEFT;
+        Direction newDirection = direction.opposite();
+        assertEquals(Direction.RIGHT, newDirection);
+    }
+
+    @Test
+    public void oppositeOfUpReturnsDown() {
+        Direction direction = Direction.UP;
+        Direction newDirection = direction.opposite();
+        assertEquals(Direction.DOWN, newDirection);
+    }
+
+    @Test
+    public void clockwiseOfLeftReturnsUp() {
+        Direction direction = Direction.LEFT;
+        Direction newDirection = direction.clockwise();
+        assertEquals(Direction.UP, newDirection);
+    }
+    @Test
+    public void counterClockwiseOfDownReturnsRight() {
+        Direction direction = Direction.DOWN;
+        Direction newDirection = direction.counterClockwise();
+        assertEquals(Direction.RIGHT, newDirection);
+    }
+
+
+}
