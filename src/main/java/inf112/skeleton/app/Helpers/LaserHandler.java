b0a2386eb49da17007d04728ecb0ca9986399a50
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-04-01 12:14:43 +0200

    changed map to remove always on laser

--- src/main/java/inf112/skeleton/app/Helpers/LaserHandler.java
+++ src/main/java/inf112/skeleton/app/Helpers/LaserHandler.java
@@ -12,29 +12,26 @@ import java.util.ArrayList;
 public class LaserHandler {
 
     public static void fireLasers(ArrayList<Position> lasers, Gamer gamer, ShapeRenderer shape, Sound pew, int TILESIZE){
-
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.setColor(Color.RED);
         Rectangle robotRectangle = new Rectangle(gamer.getSheet().getRobot().getPos().getX(), gamer.getSheet().getRobot().getPos().getY(), TILESIZE, TILESIZE);
-
+        pew.play();
         for (Position laser : lasers) {
             TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) gamer.getMap().getLayers().get(0)).getCell(laser.getX() / 64, laser.getY() / 64);
             int id = cell.getTile().getId();
             if (id == 929) { //
                 Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 4 - (64 * 3), laser.getY() + TILESIZE / 4, 64 * 3, 10);
-                shape.rect(laser.getX() + TILESIZE / 4 - (64 * 3), laser.getY() + TILESIZE / 4, 64 * 3, 10);
+                shape.rect(laser.getX() + TILESIZE / 4 - (64 * 3), laser.getY() + TILESIZE / 3, 64 * 3, 7);
                 if (laserRect.overlaps(robotRectangle)) {
                     gamer.getSheet().damageRobot();
-                    pew.play();
                 }
             }
             if (id == 646) {
                 Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 10, 64 * 3);
 
-                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 10, 64 * 3);
+                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 7, 64 * 3);
                 if (laserRect.overlaps(robotRectangle)) {
                     gamer.getSheet().damageRobot();
-                    pew.play();
                 }
 
             }
