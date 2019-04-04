ee2e3a92b2ac757d38faa4ee789c5445dc522e80
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-04-04 13:57:23 +0200

    created 2d scene programsheet

--- src/main/java/inf112/skeleton/app/Screens/MainMenuScreen.java
+++ src/main/java/inf112/skeleton/app/Screens/MainMenuScreen.java
@@ -6,11 +6,11 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -22,7 +22,6 @@ public class MainMenuScreen implements Screen {
     Viewport viewport;
 
     Stage stage;
-    Table table;
     Skin skin;
 
     public MainMenuScreen(final RoboRally game) {
@@ -45,18 +44,93 @@ public class MainMenuScreen implements Screen {
         this.stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+        Table table = new Table();
+        table.setDebug(true);
 
-        table = new Table();
-        Label nameLabel = new Label("Name:", skin);
-        TextField nameText = new TextField("", skin);
-        Label addressLabel = new Label("Address:", skin);
-        TextField addressText = new TextField("", skin);
-        table.add(nameLabel);
-        table.add(nameText).width(100);
+        Table damageTable = new Table();
+        Image damageRed = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damagered.png"))));
+        Image damageOff = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageoff.png"))));
+        Image damageOn1 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn2 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn3 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn4 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn5 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn6 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        Image damageOn7 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/damageon.png"))));
+        damageTable.add(damageRed).width(55).height(55);
+        damageTable.add(damageOff).width(55).height(55);
+        damageTable.add(damageOn1).width(55).height(55);
+        damageTable.add(damageOn2).width(55).height(55);
+        damageTable.add(damageOn3).width(55).height(55);
+        damageTable.add(damageOn4).width(55).height(55);
+        damageTable.add(damageOn5).width(55).height(55);
+        damageTable.add(damageOn6).width(55).height(55);
+        damageTable.add(damageOn7).width(55).height(55);
+        table.add(damageTable);
         table.row();
-        table.add(addressLabel);
-        table.add(addressText).width(100);
 
+        Table lifeTable = new Table();
+        Image lifeOn1 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/lifeon.png"))));
+        Image lifeOn2 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/lifeon.png"))));
+        Image lifeOff = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/lifeoff.png"))));
+        Image powerdownon = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/powerdownon.png"))));
+
+        lifeTable.add(lifeOff).width(100).height(100);
+        lifeTable.add(lifeOn1).width(100).height(100);
+        lifeTable.add(lifeOn2).width(100).height(100);
+        lifeTable.add(new Image()).width(100).height(100);
+        lifeTable.add(powerdownon).width(100).height(100);
+
+        table.add(lifeTable);
+        table.row();
+
+        Table cardTable = new Table();
+        table.add(cardTable);
+        Image image1 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image2 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image3 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image4 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image5 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image11 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image22 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image33 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image44 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image55 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image111 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image222 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image333 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image444 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+        Image image555 = new Image(new TextureRegionDrawable(new Texture(Gdx.files.internal("assets/img/card.png"))));
+
+        cardTable.add(image1);
+        cardTable.add(image2);
+        cardTable.add(image3);
+        cardTable.add(image4);
+        cardTable.add(image5);
+        table.row();
+        Label robotType = new Label("Robot type: normal", skin);
+        table.add(robotType);
+        table.row();
+        Label selectCards = new Label("Select cards: ", skin);
+        table.add(selectCards);
+        table.row();
+        Table topCards = new Table();
+        table.add(topCards);
+        topCards.add(image11);
+        topCards.add(image22);
+        topCards.add(image33);
+        topCards.add(image44);
+        topCards.add(image55);
+        table.row();
+
+        Table bottomCards = new Table();
+        table.add(bottomCards);
+        bottomCards.add(image111);
+        bottomCards.add(image222);
+        bottomCards.add(image333);
+        bottomCards.add(image444);
+
+        table.row();
 
         table.setFillParent(true);
         stage.addActor(table);
@@ -76,6 +150,9 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(camera.combined);
+
+        stage.draw();
+/*
         game.batch.begin();
 
         game.font.draw(game.batch, "" + Gdx.graphics.getWidth() + ", " + Gdx.graphics.getHeight(), 20, 20);
@@ -85,8 +162,9 @@ public class MainMenuScreen implements Screen {
         game.font.draw(game.batch, "Use numbers 1-9 to select cards", 200, 40);
         game.font.draw(game.batch, "Press enter to play cards, press space to play a blank card", 200, 60);
         game.batch.end();
+*/
 
-        if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new GameScreen(game));
             dispose();
         }
