a8aca10e4d0477a4b94c5a211a0c9ffce59d7905
ingridliabakk <ier008@uib.no>
2019-04-11 11:29:30 +0200

    fixed lasers

--- src/main/java/inf112/skeleton/app/Helpers/LaserHandler.java
+++ src/main/java/inf112/skeleton/app/Helpers/LaserHandler.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class LaserHandler {
 
-    public static void fireLasers(ArrayList<Position> lasers, Gamer gamer, ShapeRenderer shape, Sound pew, int TILESIZE){
+    public static void fireLasers(ArrayList<Position> lasers, Gamer gamer, ShapeRenderer shape, Sound pew, int TILESIZE) {
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.setColor(Color.RED);
         Rectangle robotRectangle = new Rectangle(gamer.getSheet().getRobot().getPos().getX(), gamer.getSheet().getRobot().getPos().getY(), TILESIZE, TILESIZE);
@@ -20,31 +20,22 @@ public class LaserHandler {
             TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) gamer.getMap().getLayers().get(0)).getCell(laser.getX() / 64, laser.getY() / 64);
             int id = cell.getTile().getId();
             if (id == 929) { //
-                shape.rect(laser.getX()+(TILESIZE/2), laser.getY()+(TILESIZE/2)-5, -(TILESIZE*3), 10);
-                Rectangle laserRect = new Rectangle(laser.getX()+TILESIZE-1, laser.getY(), -(TILESIZE*4), TILESIZE-1);
-                //laserRect.
-
-
-
-                System.out.println("robot is: " + robotRectangle.x + ", " + robotRectangle.y);
-                System.out.println("laser is: " + laserRect.x + ", " + laserRect.y);
+                shape.rect(laser.getX() + (TILESIZE / 2), laser.getY() + (TILESIZE / 2) - 5, -(TILESIZE * 3), 10);
+                Rectangle laserRect = new Rectangle(laser.getX() - 3 * TILESIZE, laser.getY() + (TILESIZE / 2), (TILESIZE * 4) - 1, 1);
                 if (laserRect.overlaps(robotRectangle)) {
-                    System.out.println("true");
                     gamer.getSheet().damageRobot();
                 }
 
             }
-/*
             if (id == 646) {
-                Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 10, 64 * 3);
+                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 2, 10, -(TILESIZE * 3));
+                Rectangle laserRect = new Rectangle(laser.getX() + TILESIZE / 2, laser.getY() - TILESIZE * 3, 1, (TILESIZE * 4) - 1);
 
-                shape.rect(laser.getX() + TILESIZE / 2, laser.getY() + TILESIZE / 4 - (64 * 3), 7, 64 * 3);
                 if (laserRect.overlaps(robotRectangle)) {
                     gamer.getSheet().damageRobot();
                 }
 
             }
-*/
         }
         shape.end();
 
