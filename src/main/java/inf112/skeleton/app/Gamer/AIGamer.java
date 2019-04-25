49abe362efe0eb6ae63d72279acad032993509d6
ingridliabakk <ier008@uib.no>
2019-04-25 11:21:36 +0200

    refactored Gamer class

--- src/main/java/inf112/skeleton/app/Gamer/AIGamer.java
+++ src/main/java/inf112/skeleton/app/Gamer/AIGamer.java
@@ -8,56 +8,10 @@ import inf112.skeleton.app.ProgramSheet.ProgramSheet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AIGamer implements IGamer{
-    private TiledMap map;
-    private ProgramSheet sheet;
-    private String name;
-    private int playerNumber;
-    private CardState cardState;
-
-    private List<AbstractCard> cardsOnHand;
+public class AIGamer extends AbstractGamer {
 
     public AIGamer(TiledMap map, String name, int playerNumber) {
-        this.map = map;
-        this.name = name;
-        this.playerNumber = playerNumber;
-        sheet = new ProgramSheet(map, playerNumber);
-        this.cardState = CardState.NOCARDS;
+        super(map, name, playerNumber);
+
     }
-    public ProgramSheet getSheet() {
-        return sheet;
-    }
-
-
-    public CardState getCardState() {
-        return cardState;
-    }
-
-    @Override
-    public TiledMap getMap() {
-        return this.map;
-    }
-
-    @Override
-    public void resetCards() {
-        cardsOnHand = new ArrayList<>();
-    }
-
-    @Override
-    public void setCards(List<AbstractCard> cardsOnHand) {
-        this.cardsOnHand = cardsOnHand;
-    }
-
-    @Override
-    public AbstractCard getCard(int i) {
-        return this.cardsOnHand.get(i);
-    }
-
-
-    @Override
-    public void setCardState(CardState cardState) {
-        this.cardState = cardState;
-    }
-
-
 }
--- src/main/java/inf112/skeleton/app/Gamer/Gamer.java
+++ src/main/java/inf112/skeleton/app/Gamer/Gamer.java
@@ -9,49 +9,15 @@ import inf112.skeleton.app.ProgramSheet.ProgramSheet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Gamer implements IGamer{
-
-    private TiledMap map;
-    private ProgramSheet sheet;
-    private String name;
-    private int playerNumber;
-    private List<AbstractCard> cardsOnHand;
-    private CardState cardState;
-
+public class Gamer extends AbstractGamer {
 
 
     public Gamer(TiledMap map, String name, int playerNumber) {
-        this.map = map;
-        this.name = name;
-        this.playerNumber = playerNumber;
-        sheet = new ProgramSheet(map, playerNumber);
-        this.cardState = CardState.NOCARDS;
+        super(map, name, playerNumber);
+
     }
-
-    public CardState getCardState() {
-        return cardState;
-    }
-    public void setCards(List<AbstractCard> cardsOnHand) {
-        this.cardsOnHand = cardsOnHand;
-    }{}
-
-    public AbstractCard getCard(int i){return this.cardsOnHand.get(i);}
-
-    @Override
-    public void setCardState(CardState cardState) {
-        this.cardState = cardState;
-    }
-
-    public void resetCards(){cardsOnHand = new ArrayList<>();}
 
     public List<AbstractCard> getCards() {
-        return this.cardsOnHand;
-    }
-
-    public ProgramSheet getSheet() {
-        return sheet;
-    }
-    public TiledMap getMap(){
-        return map;
+        return cardsOnHand;
     }
 }
