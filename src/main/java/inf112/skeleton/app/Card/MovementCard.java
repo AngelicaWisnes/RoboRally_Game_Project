5a7dd5a2cc6bf18f590e678e2f693736af803300
katarinaekren <katarina.ekren@student.uib.no>
2019-05-02 11:38:57 +0200

    Made javadoc for MovementCard

--- src/main/java/inf112/skeleton/app/Card/MovementCard.java
+++ src/main/java/inf112/skeleton/app/Card/MovementCard.java
@@ -3,12 +3,21 @@ package inf112.skeleton.app.Card;
 public abstract class MovementCard extends AbstractCard {
     protected int steps;
 
+    /**
+     *
+     * @param priority the cards priority
+     * @param steps number of steps you can take with a card
+     */
     public MovementCard(int priority, int steps) {
         super(priority);
         if (steps > 3 || steps < 1){throw new IllegalStateException();}
         this.steps = steps;
     }
 
+    /**
+     *
+     * @return steps 
+     */
     public int getSteps() {
         return this.steps;
     }
