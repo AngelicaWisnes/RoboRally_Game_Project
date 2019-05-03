04f500299d43c6e3e01741e57a6b70f69ad71c25
Stefan Ellenberger <Stefan.Ellenberger@uib.no>
2019-05-03 14:37:07 +0200

    fixed look of cards

--- src/main/java/inf112/skeleton/app/Views/CardView.java
+++ src/main/java/inf112/skeleton/app/Views/CardView.java
@@ -19,20 +19,23 @@ public class CardView {
             return;
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.setColor(Color.WHITE);
-        shape.rect(1160 + 80 * pos, 605, 130/2, 180/2);
+        shape.rect((1130 + 90 * pos) , 605, 80, 90);
+
         shape.end();
 
         batch.begin();
-        batch.draw(textureMap.get(card.getKey()), 1170 + 80 * pos, 645, 90/2, 90/2);
+        batch.draw(textureMap.get(card.getKey()), (1145 + 90 * pos), 645, 45, 45);
 
-        font.setColor(Color.RED);
+        font.setColor(Color.BLACK);
         GlyphLayout glyphLayout = new GlyphLayout();
         glyphLayout.setText(font, "" + card.getPriority());
-        font.draw(batch, glyphLayout, (2250  - glyphLayout.width)/2 + (80 * pos + 70), 642);
-
+        font.draw(batch, glyphLayout, (1155 + 90 * pos), 642);
         glyphLayout = new GlyphLayout();
         glyphLayout.setText(font,card.getDescription());
-        font.draw(batch, glyphLayout, (2250  - glyphLayout.width)/2 + (80 * pos + 70), 622);
+        font.draw(batch, glyphLayout, (1132 + 90 * pos), 622);
+
+
+
 
         batch.end();
     }
@@ -44,25 +47,23 @@ public class CardView {
         int x = 0;
         int y = 0;
         if (pos > 4){
-            x -= 350;
+            x -= 400;
             y -= 100;
         }
         shape.begin(ShapeRenderer.ShapeType.Filled);
         shape.setColor(Color.WHITE);
-        shape.rect((1170 + 75 * pos) + x, 250 + y, 65, 90);
+        shape.rect((1130 + 90 * pos) + x, 250 + y, 80, 90);
         shape.end();
 
         batch.begin();
-        //arrows
-        batch.draw(textureMap.get(card.getKey()), (1175 + 75 * pos) + x, 285 + y, 45, 45);
-
+        batch.draw(textureMap.get(card.getKey()), (1145 + 90 * pos) + x, 285 + y, 45, 45);
         font.setColor(Color.BLACK);
         GlyphLayout glyphLayout = new GlyphLayout();
         glyphLayout.setText(font, "" + card.getPriority());
-        font.draw(batch, glyphLayout, (1175 + 75 * pos) + x, 275 + y);
+        font.draw(batch, glyphLayout, (1155 + 90 * pos) + x, 280 + y);
         glyphLayout = new GlyphLayout();
         glyphLayout.setText(font,card.getDescription());
-        font.draw(batch, glyphLayout, (1175 + 75 * pos) + x, 265 + y);
+        font.draw(batch, glyphLayout, (1132 + 90 * pos) + x, 265 + y);
         batch.end();
     }
 }
